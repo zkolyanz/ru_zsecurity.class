44aecaac1717923074757d87cc5e09db4efd40b9
Nikolas <zkolyanz@users.noreply.github.com>
2016-07-17 17:02:25 +0300

    Update zsecurity.class.php

--- zsecurity.class.php
+++ zsecurity.class.php
@@ -1,83 +1,55 @@
 <?php
 /*
 Класс фильтрации/валидации данных, полученых различными методами.
-Создан - 15.07.2016 года
-Обновлен - 16.07.2016 года
+Версия - 1.0.1 , Создан - 15.07.2016 года , Обновлен - 17.07.2016 года
 Автор - ZKolyaNZ
-Сайт - http://zkolyanz.name/
+Сайт - https://zkolyanz.name/
 GitHub - https://github.com/zkolyanz/ru_zsecurity.class
+FAQ - https://forum.antichat.ru/threads/443271/
 */
 
 class Zsecurity { # название класса
    public $data=NULL; // публичное свойство, любые входящие данные
    
-   public function __construct(){ # функция создания экземпляра
-   }
-   
-   public function __destruct(){ # функция уничтожения экземпляра
-   }
-   
    public function Zlogin($data,$mask="^[a-zA-Zа-яА-Я0-9\-_\+]",$len="{4,20}"){ 
-   # функция проверки/валидации логина, безопасен sql inj/xss
+   # функция проверки/валидации логина
    return (is_array($data)) ? false : ( !preg_match("/{$mask}{$len}$/", $data)) ? false : trim($data);
-   unset($data,$mask,$len); // уничтожаем временные данные
    }
    
    public function Zpass($data,$mask="^[a-zA-Zа-яА-Я0-9\-_\+.,!?@#$%^&*():;~|<>'`]",$len="{4,20}"){ 
-   # функция проверки/валидации пароля, в конце данные md5() или sha1(), есть уязвимость sql inj/xss
+   # функция проверки/валидации пароля, в конце данные md5() или sha1()
    return (is_array($data)) ? false : ( !preg_match("/{$mask}{$len}$/", $data)) ? false : trim($data);
-   unset($data,$mask,$len); // уничтожаем временные данные
    }
    
    public function Zemail($data,$mask = "^([a-zа-я0-9\+_\-]+)(\.[a-zа-я0-9\+_\-]+)*@([a-zа-я0-9\-]+\.)+[a-zа-я]{2,8}"){
-   # функция проверки/валидации емейл-адреса, безопасен sql inj/xss
+   # функция проверки/валидации емейл-адреса
    if(is_array($data) && empty($data) && strlen($data) > 255 && strpos($data,'@') > 64) return false;
    return ( !preg_match("/{$mask}$/ix", $data)) ? false : strtolower(trim($data));
-   unset($data,$mask); // уничтожаем временные данные			
    }
    
    public function Zurl($data,$mask="\b(?:(?:https?|ftp):\/\/|www\.)[-a-zа-я0-9+&@#\/%?=~_|!:,.;]*[-a-zа-я0-9+&@#\/%=~_|]"){
-   # функция для проверки/валидации адреса сайта, безопасен sql inj/xss
-   $data=$this->Zxss($data); //чистим от xss
+   # функция для проверки/валидации адреса сайта
    return (is_array($data)) ? false : ( !preg_match("/{$mask}/i",$data) ) ? false : trim($data);
-   unset($data,$mask); // уничтожаем временные данные
    }
    
    public function Zonlytext($data,$mask="^[a-zA-Zа-яА-Я0-9\-_\+]"){
-   # функция для работы с получеными данными, безопасен sql inj/xss
+   # функция для работы с получеными данными
    return (is_array($data)) ? false : ( !preg_match("/{$mask}$/i", $data)) ? false : trim($data);
-   unset($data,$mask,$len); // уничтожаем временные данные
    }
    
    public function Zhtml($data){
-   # функция для подготовки вставки html в базу данных, есть вероятность sql inj
-   $data=$this->Zxss($data); //чистим от xss
+   # функция для подготовки вставки html в базу данных
    $array_find = array("<?", "?>"); // делаем php-код читабельным
    $array_replace = array("&#60;&#63;", "&#63;&#62;");
    return str_replace($array_find, $array_replace, $data);
-   unset($data,$array_find,$array_replace); // уничтожаем временные данные
    }
    
    public function Znohtml($data){ 
-   # функция для подготовки вставки в базу, в теории и xss перестает тоже работать, вероятность sql inj
-   $array_find = array("<", ">");
+   # функция для подготовки вставки в базу
+   $array_find = array("<", ">"); // делаем практически любой код читабельным
    $array_replace = array("&#60;", "&#62;");
    return str_replace($array_find, $array_replace, $data);
-   unset($data,$array_find,$array_replace); // уничтожаем временные данные
-   }
-   
-   /*private*/ public function Zsafedb($data){
-   # функция для вставки в базу данных, экранизирование  ' " \ и NULL байт, 
-   return (is_array($data)) ? false : ( addslashes($data)) ? $data : false;
-   unset($data); // уничтожаем временные данные
-   }
-   
-   /*private*/ public function Zxss($data){
-   # функция для борьбы с xss уязвимостью, начальный уровень
-   $search=array("'<script[^>]*?>.*?</script>'si","'alert\('i","'javascript:'i","'data:'i","'vbscript:'i","'livescript:'i");
-   $replace=array("","","","","","");
-   return preg_replace($search, $replace, $data);
-   unset($search,$replace,$data); // уничтожаем временные данные
    }
    
 }
+?>
